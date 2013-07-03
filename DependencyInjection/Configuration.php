eb27c80be6d4a8bbe10be6be89ff92a3c284eb3e
tautvilas <mecinskas@gmail.com>
2013-07-03 12:05:42 +0300

    update

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('mediapark_lt_cms');
+        $rootNode = $treeBuilder->root('mediapark_cms');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
